acb02fa3fac00e86150dd2a9d37c2c7f6ef98649
Vincent Young <missuo@pm.me>
2024-07-16 05:07:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,8 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.missuo.me": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "hub.owo.nz": dockerHub,
+  "ghcr.owo.nz": "https://ghcr.io",
 };
 
 function routeByHosts(host) {
